7e8de498c2c335650a070733b960ce5fb38c8f90
David Christino <davidchristino@hotmail.com>
2025-05-31 23:48:57 -0300

    Login Page - starting

---
+++ validation.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const firstname_input = document.getElementById('firstname_input');
+const email_input = document.getElementById('email_input')
+const password_input = document.getElementById('password_input')
